d1df4da2546f4f711cd42604a2fdb1dc0910bd35
Arijit <47138517+arijit05@users.noreply.github.com>
2024-02-23 14:02:46 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,8 @@
+variable "location" {
+  description = "Azure region to use."
+  type        = string
+}
+variable "rg_name" {
+  description = "Name of the resource group"
+  type        = string
+}
